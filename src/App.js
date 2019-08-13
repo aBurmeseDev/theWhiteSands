7709968d6e17be173329aef0bc5ea93edfc8b4ac
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-08-13 00:33:39 -0700

    add routers for component pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 // import './App.css';
 
+// component routes
+import Home from './components/Home';
+import Rooms from './components/Rooms';
+import SingleRoom from './components/SingleRoom';
+import Error from './components/Error';
+
+import { Route, Switch } from 'react-router-dom';
+
 function App() {
-  return <>hello react</>;
+  return (
+    <>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/rooms/' component={Rooms} />
+      <Route exact path='/rooms/:sgl' component={SingleRoom} />
+    </>
+  );
 }
 
 export default App;
