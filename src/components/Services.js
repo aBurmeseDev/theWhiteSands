3d6687cb452c57530ad2fd470bc914d94f873ebf
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-08-18 14:23:33 -0700

    add services icons and info

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
 import Title from './Title';
 
 export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: 'free cocktails',
+        info:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, nam!'
+      },
+      {
+        icon: <FaHiking />,
+        title: 'Endless Hiking',
+        info:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, nam!'
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: 'Free Shuttle',
+        info:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, nam!'
+      },
+      {
+        icon: <FaBeer />,
+        title: 'Special Brews',
+        info:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, nam!'
+      }
+    ]
+  };
   render() {
     return (
-      <div>
+      <section className='services'>
         <Title title='services' />
-      </div>
+        <div className='services-center'>
+          {this.state.services.map((item, i) => {
+            return (
+              <article key={i} className='services'>
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
     );
   }
 }
