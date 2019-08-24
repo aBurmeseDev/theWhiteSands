9f908003c1c4f30e7ec02a9ab4adde4e51d9a055
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-08-23 23:56:50 -0700

    add context to featuredRoom

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { RoomContext } from '../Context';
 
 export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
   render() {
-    return <div>hello from the FeaturedRooms</div>;
+    const value = this.context;
+    console.log(value);
+    return <div>hello from the FeaturedRooms {value}</div>;
   }
 }
